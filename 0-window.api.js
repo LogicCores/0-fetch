a2b170173ff6cdb22ea24c873a360f33a3580b36
Christoph Dorn <christoph@christophdorn.com>
2015-09-15 00:27:00 -0700

    wip

--- 0-window.api.js
+++ 0-window.api.js
@@ -1,27 +1,34 @@
 
-// TODO: Load adapters as needed on demand
-
-exports.adapters = {
-    window: require("./for/window/0-window.api")
-}
-
-exports.forContexts = function (contexts) {
+exports.forLib = function (LIB) {
 
     var exports = {};
 
-    var Context = exports.Context = function (defaults) {
-        var self = this;
-    
-        var state = window._.extend({
-        }, defaults || {});
+    // TODO: Load adapters as needed on demand
 
-
-        self.resolveUri = function () {
-            return contexts.page.resolveUri.apply(contexts.page, arguments);
-        }
+    exports.adapters = {
+        window: require("./for/window/0-window.api").forLib(LIB)
     }
-    Context.prototype = Object.create(window.EventEmitter.prototype);
-    Context.prototype.contexts = contexts;
+
+    exports.forContexts = function (contexts) {
+    
+        var exports = {};
+    
+        var Context = exports.Context = function (defaults) {
+            var self = this;
+        
+            var state = LIB._.extend({
+            }, defaults || {});
+    
+    
+            self.resolveUri = function () {
+                return contexts.page.resolveUri.apply(contexts.page, arguments);
+            }
+        }
+        Context.prototype = Object.create(LIB.EventEmitter.prototype);
+        Context.prototype.contexts = contexts;
+    
+        return exports;
+    }
 
     return exports;
 }
--- for/window/0-window.api.js
+++ for/window/0-window.api.js
@@ -1,28 +1,35 @@
 
-exports.spin = function (context) {
+exports.forLib = function (LIB) {
+    
+    var exports = {};
 
-    var Fetch = function () {
-        var self = this;
+    exports.spin = function (context) {
+    
+        var Fetch = function () {
+            var self = this;
+        }
+        Fetch.prototype.fetch = function (uri, options) {
+            options = options || {};
+    
+            var url = context.resolveUri(uri);
+    
+            var init = {
+                headers: {
+                    // Will cause 403 to return instead of redirect when unauthorized
+                    "X-Request-Type": "background-fetch"
+                }
+            };
+            LIB._.merge(init, {
+                // Send cookies by default
+                credentials: 'same-origin'
+            });
+            LIB._.merge(init, options);
+    
+            return window.fetch(url, init);
+        }
+    
+        return new Fetch(context);
     }
-    Fetch.prototype.fetch = function (uri, options) {
-        options = options || {};
 
-        var url = context.resolveUri(uri);
-
-        var init = {
-            headers: {
-                // Will cause 403 to return instead of redirect when unauthorized
-                "X-Request-Type": "background-fetch"
-            }
-        };
-        window._.merge(init, {
-            // Send cookies by default
-            credentials: 'same-origin'
-        });
-        window._.merge(init, options);
-
-        return window.fetch(url, init);
-    }
-
-    return new Fetch(context);
+    return exports;
 }
